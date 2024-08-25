bdd67cb9bb2337a7ea2166031d22866fec4ab109
chenshenchao <chenshenchao@outlook.com>
2024-08-25 19:46:47 +0800

    CONF.

--- cmake/Redis.cmake
+++ cmake/Redis.cmake
@@ -1,4 +1,8 @@
-include(ExternalProject)
+﻿include(ExternalProject)
+
+set(REDIS_GIT_REPOSITORY "git@github.com:redis/redis.git" CACHE STRING "redis git link.")
+set(REDIS_GIT_TAG "7.0.14" CACHE STRING "redis git tag.")
+
 
 find_program(MSYS2_SHELL NAMES msys2_shell.cmd msys2_shell)
 message(STATUS "msys2_shell: ${MSYS2_SHELL}")
@@ -7,6 +11,7 @@ message(STATUS "msys2: ${MSYS2_DIR}")
 set(MSYS2_DLL_PATH "${MSYS2_DIR}/usr/bin/msys-2.0.dll")
 message(STATUS "msys2 dll: ${MSYS2_DLL_PATH}")
 
+# ExternalProject 的方式编译 redis
 function(
     BuildRedisOnWindows
     REDIS_INSTALL_DIR
@@ -18,9 +23,10 @@ function(
     # 编译 redis
     ExternalProject_Add(
         redis_bin
-        GIT_REPOSITORY git@github.com:redis/redis.git
-        GIT_TAG 7.0.14
+        GIT_REPOSITORY ${REDIS_GIT_REPOSITORY}
+        GIT_TAG ${REDIS_GIT_TAG}
         GIT_SHALLOW ON
+        GIT_PROGRESS ON
 
         CONFIGURE_COMMAND ""
         BUILD_COMMAND ${MSYS2_SHELL} -no-start -defterm -mingw64 -where ${REDIS_SOURCE_DIR} -shell bash -c "make CFLAGS='-D__GNU_VISIBLE -w' PREFIX=${REDIS_INSTALL_DIR} install"
@@ -39,4 +45,25 @@ function(
     )
 
     add_dependencies(redis redis_bin)
+endfunction()
+
+include(FetchContent)
+
+# TODO FetchContent 方式编译 redis
+function(
+    FetchRedis
+)
+    # 编译 redis
+    FetchContent_Declare(
+        redis_bin
+        GIT_REPOSITORY ${REDIS_GIT_REPOSITORY}
+        GIT_TAG ${REDIS_GIT_TAG}
+        GIT_SHALLOW ON # 浅克隆，git 参数加 --depth=1
+        GIT_PROGRESS ON # 打印下载进度
+    )
+    FetchContent_GetProperties(redis_bin)
+    if(NOT redis_bin_POPULATED)
+        FetchContent_Populate(redis_bin)
+        message(STATUS "[redis_bin]: ${redis_bin_SOURCE_DIR}")
+    endif()
 endfunction()
